20a8576853d2af4dcd631b750ad514fc7b93da33
Peter Polidoro <peterpolidoro@gmail.com>
2016-03-04 09:45:42 -0500

    Fixed bugs.

--- nodes/pose_publisher.py
+++ nodes/pose_publisher.py
@@ -13,13 +13,13 @@ def pose_publisher():
     rospy.init_node('zaber_stage_pose_publisher')
     rospy.loginfo('zaber_stage pose_publisher...')
     rate = rospy.Rate(4)
-    pub = rospy.Publisher('~pose',Pose,queue_size=10)
-    rospy.wait_for_service('~get_pose')
-    get_pose = rospy.ServiceProxy('~get_pose',GetPose)
+    pub = rospy.Publisher('/zaber_stage_node/pose',Pose,queue_size=10)
+    rospy.wait_for_service('/zaber_stage_node/get_pose')
+    get_pose = rospy.ServiceProxy('/zaber_stage_node/get_pose',GetPose)
     while not rospy.is_shutdown():
         try:
-            pose = get_pose()
-            pub.publish(pose)
+            response = get_pose()
+            pub.publish(response.pose)
         except rospy.ServiceException, e:
             print "Service call failed: %s"%e
         rate.sleep()
